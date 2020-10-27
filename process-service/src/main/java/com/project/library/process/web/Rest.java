88a2217685746710eef42217723e193d929b93f4
robin <robinforsman@hotmail.com>
2020-10-27 11:57:26 +0100

    Fixed whole composite file

--- process-service/src/main/java/com/project/library/process/web/Rest.java
+++ process-service/src/main/java/com/project/library/process/web/Rest.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/order-process")
+@RequestMapping("api/v1/orders")
 public class Rest {
     private OrderService orderService;
 
